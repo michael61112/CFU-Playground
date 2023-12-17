b231dd11f01d10553097cd4dc01915d2620c1cd3
michael61112 <michael61112@yahoo.com.tw>
2023-12-17 13:24:14 +0800

    Lab5: Replace the data width by parameter of systolic_array.v

--- proj/lab5/RTL/systolic_array.v
+++ proj/lab5/RTL/systolic_array.v
@@ -27,8 +27,7 @@ SOFTWARE.
 //`include "block.v"
 module systolic_array
 #(  parameter ADDR_BITS=16, 
-	parameter DATA_BITS=32,
-	parameter DATAC_BITS=128,
+	parameter DATA_BITS=16,
 	parameter S0 = 3'b000,
 	parameter S1 = 3'b001,
 	parameter S2 = 3'b010,
@@ -46,30 +45,30 @@ module systolic_array
 	input busy,
 	output done,
 
-	output [7:0] inp_north0_o,
-	output [7:0] inp_north1_o,
-	output [7:0] inp_north2_o,
-	output [7:0] inp_north3_o,
-	output [7:0] inp_west0_o, 
-	output [7:0] inp_west4_o, 
-	output [7:0] inp_west8_o, 
-	output [7:0] inp_west12_o,
-	output [31:0] result0_o,
+	output [DATA_BITS-1:0] inp_north0_o,
+	output [DATA_BITS-1:0] inp_north1_o,
+	output [DATA_BITS-1:0] inp_north2_o,
+	output [DATA_BITS-1:0] inp_north3_o,
+	output [DATA_BITS-1:0] inp_west0_o,
+	output [DATA_BITS-1:0] inp_west4_o,
+	output [DATA_BITS-1:0] inp_west8_o,
+	output [DATA_BITS-1:0] inp_west12_o,
+	output [DATA_BITS*2-1:0] result0_o,
 
-	input [DATA_BITS-1:0] local_buffer_A0,
-	input [DATA_BITS-1:0] local_buffer_A1,
-	input [DATA_BITS-1:0] local_buffer_A2,
-	input [DATA_BITS-1:0] local_buffer_A3,
+	input [DATA_BITS*4-1:0] local_buffer_A0,
+	input [DATA_BITS*4-1:0] local_buffer_A1,
+	input [DATA_BITS*4-1:0] local_buffer_A2,
+	input [DATA_BITS*4-1:0] local_buffer_A3,
 
-	input [DATA_BITS-1:0] local_buffer_B0,
-	input [DATA_BITS-1:0] local_buffer_B1,
-	input [DATA_BITS-1:0] local_buffer_B2,
-	input [DATA_BITS-1:0] local_buffer_B3,
+	input [DATA_BITS*4-1:0] local_buffer_B0,
+	input [DATA_BITS*4-1:0] local_buffer_B1,
+	input [DATA_BITS*4-1:0] local_buffer_B2,
+	input [DATA_BITS*4-1:0] local_buffer_B3,
 
-	output [DATAC_BITS-1:0] local_buffer_C0,
-	output [DATAC_BITS-1:0] local_buffer_C1,
-	output [DATAC_BITS-1:0] local_buffer_C2,
-	output [DATAC_BITS-1:0] local_buffer_C3
+	output [DATA_BITS*2*4-1:0] local_buffer_C0,
+	output [DATA_BITS*2*4-1:0] local_buffer_C1,
+	output [DATA_BITS*2*4-1:0] local_buffer_C2,
+	output [DATA_BITS*2*4-1:0] local_buffer_C3
 );
 
 	reg [3:0] count;
@@ -78,11 +77,11 @@ module systolic_array
 	
 	assign state_SA_o = state;
 	assign result0_o = result7;
-	wire [7:0] inp_north0, inp_north1, inp_north2, inp_north3;
-	wire [7:0] inp_west0, inp_west4, inp_west8, inp_west12;
-	wire [7:0] outp_south0, outp_south1, outp_south2, outp_south3, outp_south4, outp_south5, outp_south6, outp_south7, outp_south8, outp_south9, outp_south10, outp_south11, outp_south12, outp_south13, outp_south14, outp_south15;
-	wire [7:0] outp_east0, outp_east1, outp_east2, outp_east3, outp_east4, outp_east5, outp_east6, outp_east7, outp_east8, outp_east9, outp_east10, outp_east11, outp_east12, outp_east13, outp_east14, outp_east15;
-	wire [31:0] result0, result1, result2, result3, result4, result5, result6, result7, result8, result9, result10, result11, result12, result13, result14, result15;
+	wire [DATA_BITS-1:0] inp_north0, inp_north1, inp_north2, inp_north3;
+	wire [DATA_BITS-1:0] inp_west0, inp_west4, inp_west8, inp_west12;
+	wire [DATA_BITS-1:0] outp_south0, outp_south1, outp_south2, outp_south3, outp_south4, outp_south5, outp_south6, outp_south7, outp_south8, outp_south9, outp_south10, outp_south11, outp_south12, outp_south13, outp_south14, outp_south15;
+	wire [DATA_BITS-1:0] outp_east0, outp_east1, outp_east2, outp_east3, outp_east4, outp_east5, outp_east6, outp_east7, outp_east8, outp_east9, outp_east10, outp_east11, outp_east12, outp_east13, outp_east14, outp_east15;
+	wire [DATA_BITS*2-1:0] result0, result1, result2, result3, result4, result5, result6, result7, result8, result9, result10, result11, result12, result13, result14, result15;
 	// debug
 
 	assign inp_north0_o = inp_north0;
@@ -97,25 +96,25 @@ module systolic_array
 	wire rst;
 	assign rst = ~sa_rst_n;
 	
-	block P0 (inp_north0, inp_west0, clk, rst, outp_south0, outp_east0, result0);
-	block P1 (inp_north1, outp_east0, clk, rst, outp_south1, outp_east1, result1);
-	block P2 (inp_north2, outp_east1, clk, rst, outp_south2, outp_east2, result2);
-	block P3 (inp_north3, outp_east2, clk, rst, outp_south3, outp_east3, result3);
+	block #(DATA_BITS) P0 (inp_north0, inp_west0, clk, rst, outp_south0, outp_east0, result0);
+	block #(DATA_BITS) P1 (inp_north1, outp_east0, clk, rst, outp_south1, outp_east1, result1);
+	block #(DATA_BITS) P2 (inp_north2, outp_east1, clk, rst, outp_south2, outp_east2, result2);
+	block #(DATA_BITS) P3 (inp_north3, outp_east2, clk, rst, outp_south3, outp_east3, result3);
 	
-	block P4 (outp_south0, inp_west4, clk, rst, outp_south4, outp_east4, result4);
-	block P5 (outp_south1, outp_east4, clk, rst, outp_south5, outp_east5, result5);
-	block P6 (outp_south2, outp_east5, clk, rst, outp_south6, outp_east6, result6);
-	block P7 (outp_south3, outp_east6, clk, rst, outp_south7, outp_east7, result7);
+	block #(DATA_BITS) P4 (outp_south0, inp_west4, clk, rst, outp_south4, outp_east4, result4);
+	block #(DATA_BITS) P5 (outp_south1, outp_east4, clk, rst, outp_south5, outp_east5, result5);
+	block #(DATA_BITS) P6 (outp_south2, outp_east5, clk, rst, outp_south6, outp_east6, result6);
+	block #(DATA_BITS) P7 (outp_south3, outp_east6, clk, rst, outp_south7, outp_east7, result7);
 
-	block P8 (outp_south4, inp_west8, clk, rst, outp_south8, outp_east8, result8);
-	block P9 (outp_south5, outp_east8, clk, rst, outp_south9, outp_east9, result9);
-	block P10 (outp_south6, outp_east9, clk, rst, outp_south10, outp_east10, result10);
-	block P11 (outp_south7, outp_east10, clk, rst, outp_south11, outp_east11, result11);
+	block #(DATA_BITS) P8 (outp_south4, inp_west8, clk, rst, outp_south8, outp_east8, result8);
+	block #(DATA_BITS) P9 (outp_south5, outp_east8, clk, rst, outp_south9, outp_east9, result9);
+	block #(DATA_BITS) P10 (outp_south6, outp_east9, clk, rst, outp_south10, outp_east10, result10);
+	block #(DATA_BITS) P11 (outp_south7, outp_east10, clk, rst, outp_south11, outp_east11, result11);
 	
-	block P12 (outp_south8, inp_west12, clk, rst, outp_south12, outp_east12, result12);
-	block P13 (outp_south9, outp_east12, clk, rst, outp_south13, outp_east13, result13);
-	block P14 (outp_south10, outp_east13, clk, rst, outp_south14, outp_east14, result14);
-	block P15 (outp_south11, outp_east14, clk, rst, outp_south15, outp_east15, result15);
+	block #(DATA_BITS) P12 (outp_south8, inp_west12, clk, rst, outp_south12, outp_east12, result12);
+	block #(DATA_BITS) P13 (outp_south9, outp_east12, clk, rst, outp_south13, outp_east13, result13);
+	block #(DATA_BITS) P14 (outp_south10, outp_east13, clk, rst, outp_south14, outp_east14, result14);
+	block #(DATA_BITS) P15 (outp_south11, outp_east14, clk, rst, outp_south15, outp_east15, result15);
 	
 
 	always@(negedge clk) begin
@@ -218,100 +217,100 @@ integer addr_w0, addr_w4, addr_w8, addr_w12;
 		endcase
 	end
 
-    reg [31:0]local_buffer_A[3:0];
-    reg [31:0]local_buffer_B[3:0];
-	reg [127:0]local_buffer_C[3:0];
+    reg [DATA_BITS*4-1:0]local_buffer_A[3:0];
+    reg [DATA_BITS*4-1:0]local_buffer_B[3:0];
+	reg [DATA_BITS*2*4-1:0]local_buffer_C[3:0];
 
 	assign local_buffer_C0 = local_buffer_C[0];
 	assign local_buffer_C1 = local_buffer_C[1];
 	assign local_buffer_C2 = local_buffer_C[2];
 	assign local_buffer_C3 = local_buffer_C[3];
 
-	reg [7:0] inp_north0_temp, inp_north1_temp, inp_north2_temp, inp_north3_temp;
-	reg [7:0] inp_west0_temp, inp_west4_temp, inp_west8_temp, inp_west12_temp;
+	reg [DATA_BITS-1:0] inp_north0_temp, inp_north1_temp, inp_north2_temp, inp_north3_temp;
+	reg [DATA_BITS-1:0] inp_west0_temp, inp_west4_temp, inp_west8_temp, inp_west12_temp;
 
 	always @(*) begin
 		if(addr_n0 >= 0 && addr_n0 < 4) begin
 			case (addr_n0)
-				0: inp_north0_temp <= local_buffer_B[0][31:24];
-				1: inp_north0_temp <= local_buffer_B[1][31:24];
-				2: inp_north0_temp <= local_buffer_B[2][31:24];
-				3: inp_north0_temp <= local_buffer_B[3][31:24];
+				0: inp_north0_temp <= local_buffer_B[0][DATA_BITS*4-1:DATA_BITS*3];
+				1: inp_north0_temp <= local_buffer_B[1][DATA_BITS*4-1:DATA_BITS*3];
+				2: inp_north0_temp <= local_buffer_B[2][DATA_BITS*4-1:DATA_BITS*3];
+				3: inp_north0_temp <= local_buffer_B[3][DATA_BITS*4-1:DATA_BITS*3];
 			endcase
 		end 
 		else begin
-			inp_north0_temp <= 8'b0;
+			inp_north0_temp <= {DATA_BITS{1'b0}};
 		end 
 		if(addr_n1 >= 0 && addr_n1 < 4) begin
 			case (addr_n1)
-				0: inp_north1_temp <= local_buffer_B[0][23:16];
-				1: inp_north1_temp <= local_buffer_B[1][23:16];
-				2: inp_north1_temp <= local_buffer_B[2][23:16];
-				3: inp_north1_temp <= local_buffer_B[3][23:16];
+				0: inp_north1_temp <= local_buffer_B[0][DATA_BITS*3-1:DATA_BITS*2];
+				1: inp_north1_temp <= local_buffer_B[1][DATA_BITS*3-1:DATA_BITS*2];
+				2: inp_north1_temp <= local_buffer_B[2][DATA_BITS*3-1:DATA_BITS*2];
+				3: inp_north1_temp <= local_buffer_B[3][DATA_BITS*3-1:DATA_BITS*2];
 			endcase
 		end else begin
-			inp_north1_temp <= 8'b0;
+			inp_north1_temp <= {DATA_BITS{1'b0}};
 		end
 		if(addr_n2 >= 0 && addr_n2 < 4) begin
 			case (addr_n2)
-				0: inp_north2_temp <= local_buffer_B[0][15:8];
-				1: inp_north2_temp <= local_buffer_B[1][15:8];
-				2: inp_north2_temp <= local_buffer_B[2][15:8];
-				3: inp_north2_temp <= local_buffer_B[3][15:8];
+				0: inp_north2_temp <= local_buffer_B[0][DATA_BITS*2-1:DATA_BITS*1];
+				1: inp_north2_temp <= local_buffer_B[1][DATA_BITS*2-1:DATA_BITS*1];
+				2: inp_north2_temp <= local_buffer_B[2][DATA_BITS*2-1:DATA_BITS*1];
+				3: inp_north2_temp <= local_buffer_B[3][DATA_BITS*2-1:DATA_BITS*1];
 			endcase
 		end else begin
-			inp_north2_temp <= 8'b0;
+			inp_north2_temp <= {DATA_BITS{1'b0}};
 		end
 		if(addr_n3 >= 0 && addr_n3 < 4) begin
 			case(addr_n3)
-				0: inp_north3_temp <= local_buffer_B[0][7:0];
-				1: inp_north3_temp <= local_buffer_B[1][7:0];
-				2: inp_north3_temp <= local_buffer_B[2][7:0];
-				3: inp_north3_temp <= local_buffer_B[3][7:0];
+				0: inp_north3_temp <= local_buffer_B[0][DATA_BITS-1:0];
+				1: inp_north3_temp <= local_buffer_B[1][DATA_BITS-1:0];
+				2: inp_north3_temp <= local_buffer_B[2][DATA_BITS-1:0];
+				3: inp_north3_temp <= local_buffer_B[3][DATA_BITS-1:0];
 			endcase
 		end else begin
-			inp_north3_temp <= 8'b0;
+			inp_north3_temp <= {DATA_BITS{1'b0}};
 		end
 	///////////////////////////////////////////////////////////////
 		if(addr_w0 >= 0 && addr_w0 < 4) begin
 			case (addr_w0)
-				0: inp_west0_temp <= local_buffer_A[0][31:24];
-				1: inp_west0_temp <= local_buffer_A[1][31:24];
-				2: inp_west0_temp <= local_buffer_A[2][31:24];
-				3: inp_west0_temp <= local_buffer_A[3][31:24];
+				0: inp_west0_temp <= local_buffer_A[0][DATA_BITS*4-1:DATA_BITS*3];
+				1: inp_west0_temp <= local_buffer_A[1][DATA_BITS*4-1:DATA_BITS*3];
+				2: inp_west0_temp <= local_buffer_A[2][DATA_BITS*4-1:DATA_BITS*3];
+				3: inp_west0_temp <= local_buffer_A[3][DATA_BITS*4-1:DATA_BITS*3];
 			endcase
 		end else begin
-			inp_west0_temp <= 8'b0;
+			inp_west0_temp <= {DATA_BITS{1'b0}};
 		end
 		if(addr_w4 >= 0 && addr_w4 < 4) begin
 			case (addr_w4)
-				0: inp_west4_temp <= local_buffer_A[0][23:16];
-				1: inp_west4_temp <= local_buffer_A[1][23:16];
-				2: inp_west4_temp <= local_buffer_A[2][23:16];
-				3: inp_west4_temp <= local_buffer_A[3][23:16];
+				0: inp_west4_temp <= local_buffer_A[0][DATA_BITS*3-1:DATA_BITS*2];
+				1: inp_west4_temp <= local_buffer_A[1][DATA_BITS*3-1:DATA_BITS*2];
+				2: inp_west4_temp <= local_buffer_A[2][DATA_BITS*3-1:DATA_BITS*2];
+				3: inp_west4_temp <= local_buffer_A[3][DATA_BITS*3-1:DATA_BITS*2];
 			endcase
 		end else begin
-			inp_west4_temp <= 8'b0;
+			inp_west4_temp <= {DATA_BITS{1'b0}};
 		end
 		if(addr_w8 >= 0 && addr_w8 < 4) begin
 			case (addr_w8)
-				0: inp_west8_temp <= local_buffer_A[0][15:8];
-				1: inp_west8_temp <= local_buffer_A[1][15:8];
-				2: inp_west8_temp <= local_buffer_A[2][15:8];
-				3: inp_west8_temp <= local_buffer_A[3][15:8];
+				0: inp_west8_temp <= local_buffer_A[0][DATA_BITS*2-1:DATA_BITS*1];
+				1: inp_west8_temp <= local_buffer_A[1][DATA_BITS*2-1:DATA_BITS*1];
+				2: inp_west8_temp <= local_buffer_A[2][DATA_BITS*2-1:DATA_BITS*1];
+				3: inp_west8_temp <= local_buffer_A[3][DATA_BITS*2-1:DATA_BITS*1];
 			endcase
 		end else begin
-			inp_west8_temp <= 8'b0;
+			inp_west8_temp <= {DATA_BITS{1'b0}};
 		end
 		if(addr_w12 >= 0 && addr_w12 < 4) begin
 			case (addr_w12)
-				0: inp_west12_temp <= local_buffer_A[0][7:0];
-				1: inp_west12_temp <= local_buffer_A[1][7:0];
-				2: inp_west12_temp <= local_buffer_A[2][7:0];
-				3: inp_west12_temp <= local_buffer_A[3][7:0];
+				0: inp_west12_temp <= local_buffer_A[0][DATA_BITS-1:0];
+				1: inp_west12_temp <= local_buffer_A[1][DATA_BITS-1:0];
+				2: inp_west12_temp <= local_buffer_A[2][DATA_BITS-1:0];
+				3: inp_west12_temp <= local_buffer_A[3][DATA_BITS-1:0];
 			endcase
 		end else begin
-			inp_west12_temp <= 8'b0;
+			inp_west12_temp <= {DATA_BITS{1'b0}};
 		end
 	end
 
@@ -327,4 +326,3 @@ integer addr_w0, addr_w4, addr_w8, addr_w12;
 
 	assign done = done_temp;
 endmodule
-		      
